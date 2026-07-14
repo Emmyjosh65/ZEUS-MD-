738c404aa1edcbd36befea920af6135c89545dd6
Emmyjosh65 <ge5853987@gmail.com>
2026-07-14 20:39:42 +0100

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,17 @@
+import config from '../../config.js';
 
+export function formatNumber(number) {
+  return number.replace(/[^0-9]/g, '');
+}
+
+export function isOwner(senderNumber) {
+  return senderNumber === config.ownerNumber.replace(/[^0-9]/g, '');
+}
+
+export function getTimestamp() {
+  return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+}
+
+export function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
